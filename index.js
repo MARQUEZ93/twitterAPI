99bc4f82189078ff69369da8410a117887a40cf3
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2019-01-26 15:46:00 -0500

    render react

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log("wtf");
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to twitterAPI</h1>, root);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path');
+var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/twitterAPI.jsx',
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
+        test: [/\.js?$/, /\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
       }
     ]
   },
